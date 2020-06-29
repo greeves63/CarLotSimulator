4324806f18a5ffb0ff7d563402d95ead620a6dd1
Gregory Reeves Jr <gregoryreevesjr@gmail.com>
2020-06-29 14:32:53 -0500

    Added changes to the file

---
+++ CarLotSimulator/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //FINISHED - Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //FINISHED - Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //FINISHED - The methods should take one string parameter: the respective noise property  
+        public Car()    ////Constructor
+        {
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public string MakeEngineNoise()
+        {
+            return EngineNoise;
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(HonkNoise);
+        }
+    }
+}
--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -6,17 +6,58 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
+            //DONE
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property            
+            //DONE - Create a seperate class file called Car
+            //FINISHED - Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //FINISHED - Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //FINISHED - The methods should take one string parameter: the respective noise property            
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+
             //Set the properties for each of the cars
+            
+
+            // First Car Instantiation - Dot Notation
+            var gregsCar = new Car();
+            gregsCar.Make = "Jeep";
+            gregsCar.Model = "Patriot";
+            gregsCar.Year = 2016;
+            gregsCar.EngineNoise = "I'm Smart!!!!";
+            gregsCar.HonkNoise = "onk";
+            gregsCar.IsDriveable = true;
+
+            //Second Car - Wife's Car - Dot Notation
+            var brandisCar = new Car();
+            brandisCar.Make = "Chevy";
+            brandisCar.Model = "Equinox";
+            brandisCar.Year = 2018;
+            brandisCar.EngineNoise = "I'm A Savage!!!!";
+            brandisCar.HonkNoise = "tut-tut";
+            brandisCar.IsDriveable = true;
+
+            //Thrid Car - Second Instantiation - Object Initalizer Syntax
+            var cheronsCar = new Car()
+            {
+                Year = 1988,
+                Make = "Buick",
+                Model = "Regal",
+                EngineNoise = "...",
+                HonkNoise = "urnk...urnk",
+                IsDriveable = false,
+            };
+
+            //Fourth Car - Dream Car - Makes 3rd Instantiation - Using Constructor to allow parameters to be
+            //placed inside of properites.
+            var gregsCar2 = new Car(2020, "Mercedes", "G-Wagon", "doom..doom", "uuurrnnnkkk", true);
+
             //Call each of the methods for each car
+            gregsCar.MakeEngineNoise();
+            brandisCar.MakeEngineNoise();
+            cheronsCar.MakeEngineNoise();
+            gregsCar2.MakeEngineNoise();
+
 
             //*************BONUS*************//
 
